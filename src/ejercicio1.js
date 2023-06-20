55bdfbbc5d30a9737f389d143c2377738e69fcbe
Adaliz <jimenezvianey540@gmail.com>
2023-06-20 15:20:35 -0600

    ta

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,10 @@
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+// Inserte el código aquí
+   this.valor += 1;
+   return this.valor;
   },
 };
 
-export { contador }
+export { contador };
