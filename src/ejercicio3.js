aec18c45e40352465f26b693b6b0565f3f25577e
Adaliz <jimenezvianey540@gmail.com>
2023-06-21 13:38:32 -0600

    practica

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,26 @@
 // Inserte el código aquí
 
+//#1 Funcion ClonarArreglo
 
+
+
+//#2 Funcion ClonarObjeto
+
+
+
+//#3 Funcion Sumar
+ function sumar(... numeros){
+    return numeros.reduce((acumulador, valorActual){
+        return acumulador + valorActual
+    },0)
+ }
+const resultado = sumar (2,5,7,1)
+
+// #4 Funcion saludar 
+function saludar(nombre = 'desconocido'){
+    console.log('Hola',nombre)
+}
+saludar('Juan De Dios')
 
 // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
